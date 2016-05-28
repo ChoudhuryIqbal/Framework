cab7b9af57f98ad78e53c471c843095c1e7f9092
iqbal <iqbalchoudhury2@gmail.com>
2016-05-28 15:32:09 -0500

    poi

--- Cnn/src/main/java/reader/ReadNewsData.java
+++ Cnn/src/main/java/reader/ReadNewsData.java
@@ -1,10 +1,20 @@
 package reader;
 
+import util.DataReader;
+
+import java.io.IOException;
+
 /**
  * Created by a on 5/28/2016.
  */
 public class ReadNewsData {
-    
+    DataReader dataReader=new DataReader();
+    public String [] getData() throws IOException{
+        String path=System.getProperty("user.dir")+"/data/searchData.xls";
+
+        String[] news=dataReader.fileReader(path);
+        return news;
+    }
 
 
 }
--- Cnn/src/test/news/Search.java
+++ Cnn/src/test/news/Search.java
@@ -3,17 +3,27 @@ package news;
 import base.CommonAPI;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
+import reader.ReadNewsData;
+
+import java.io.IOException;
 
 /**
  * Created by a on 5/23/2016.
  */
 public class Search extends CommonAPI {
 
+    ReadNewsData readNewsData=new ReadNewsData();
+
+
     @Test
-    public void searchNews() throws InterruptedException{
+    public void searchNews() throws InterruptedException,IOException{
         clickByCss("#search-button");
         Thread.sleep(1000);
-        typeByCss("#search-input-field","Politics");
+
+
+
+        typeByCss("#search-input-field",readNewsData.getData());
+
         takeEnterKeys("#search-input-field");
         Thread.sleep(3000);
 
